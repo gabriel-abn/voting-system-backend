705e849c0f4b12caec5b554eaa1f7d95a7e743b8
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-28 23:34:53 -0300

    feat: :sparkles: created base entity class

---
+++ src/domain/common/entity.ts
@@ -0,0 +1,13 @@
+export abstract class Entity<T> {
+  private readonly id: number;
+  protected readonly props: T;
+
+  constructor(props: T, id: number) {
+    this.props = props;
+    this.id = id;
+  }
+
+  public getProps(): T {
+    return this.props;
+  }
+}
