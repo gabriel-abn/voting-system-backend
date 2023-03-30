37a66fbb821a9554a9fccb453c0150da49be4ca3
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-30 11:05:25 -0300

    feat(UserRepository): :art: created modular export

---
+++ src/presentation/controllers/user/index.ts
@@ -0,0 +1,6 @@
+export * from "./delete-user-controller";
+export * from "./edit-user-controller";
+export * from "./get-all-users-controller";
+export * from "./get-user-by-id-controller";
+export * from "./login-controller";
+export * from "./register-user-controller";
