30591b085e654f2f83e0de8ef0dd0155f9b5fa69
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-30 14:59:21 -0300

    feat(Infra): :art: created route adapter helper

---
+++ src/infra/http/route-adapter.ts
@@ -0,0 +1,21 @@
+import { Controller } from "@presentation/common";
+import { Request, Response } from "express";
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const request = {
+      ...(req.body || {}),
+      ...(req.params || {}),
+    };
+    const httpResponse = await controller.handle(request);
+    if (httpResponse.status >= 200 && httpResponse.status <= 299) {
+      res
+        .status(httpResponse.status)
+        .send({ response: httpResponse.body, status: httpResponse.status });
+    } else {
+      res
+        .status(httpResponse.status)
+        .send({ error: httpResponse.body, status: httpResponse.status });
+    }
+  };
+};
