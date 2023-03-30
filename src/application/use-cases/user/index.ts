6cd1868f8a1b5ac461f497ef6d8587c33b678c20
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-30 10:24:53 -0300

    feat(UserUseCase): :art: added modular export

---
+++ src/application/use-cases/user/index.ts
@@ -0,0 +1,6 @@
+export * from "./delete-user-use-case";
+export * from "./edit-user-use-case";
+export * from "./get-all-users-use-case";
+export * from "./get-user-by-id-use-case";
+export * from "./login-use-case";
+export * from "./register-user-use-case";
