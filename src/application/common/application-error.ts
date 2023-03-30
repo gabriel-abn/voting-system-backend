67a188da8b43ca7153a6b50e056608e58f3edc0a
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-30 17:01:05 -0300

    feat(Application): :art: added toString method to error classes

--- src/application/common/application-error.ts
+++ src/application/common/application-error.ts
@@ -2,4 +2,8 @@ export class ApplicationError extends Error {
   constructor(message: string) {
     super(message);
   }
+
+  public toString(): string {
+    return this.message;
+  }
 }
--- src/application/common/repository-error.ts
+++ src/application/common/repository-error.ts
@@ -3,4 +3,8 @@ export class RepositoryError extends Error {
     super(message);
     this.name = "RepositoryError";
   }
+
+  public toString(): string {
+    return `${this.name}: ${this.message}`;
+  }
 }
